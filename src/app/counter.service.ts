aecded07a19124ee132624313a21d93d905ffc39
Cheikh Ahmed Tidiane FALL <ahmadatidjane@gmail.com>
2019-11-07 19:30:14 +0100

    ajout de la fonction increment

--- src/app/counter.service.ts
+++ src/app/counter.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { Counter } from './counter';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,14 +9,20 @@ import { Injectable } from '@angular/core';
 export class CounterService {
   public initialValue = [12, 6, 78];
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
+  url = 'https://lp4asgadot.herokuapp.com/counters/'
   reset() {
     this.initialValue = [0, 0, 0];
   }
 
-  increment(position: number): number {
-    this.initialValue[position]++;
-    return this.initialValue[position];
+  increment(id:number): Observable<Counter>{
+    
+    this.httpClient.patch(this.url+id+".json",{"value" : 1}).subscribe();
+    return this.httpClient.get<Counter>(this.url+id+".json");
+}
+  
+  getCounterValue(id: number): Observable<Counter> {
+    return this.httpClient.get<Counter>(this.url+"59.json")
   }
 }
--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { CounterService } from '../counter.service';
+import { Observable } from 'rxjs';
+import { Counter } from '../counter';
+
 
 @Component({
   selector: 'app-counter',
@@ -8,15 +11,23 @@ import { CounterService } from '../counter.service';
 })
 export class CounterComponent implements OnInit {
 
-  @Input() position;
+  @Input() position: number;
+
+  value: Counter;
 
   constructor(public counterService: CounterService) { }
 
   ngOnInit() {
+    console.log("call counter")
+    this.counterService.getCounterValue(this.position)
+          .subscribe(counter => this.value = counter) 
+         
   }
 
   increment() {
-    this.counterService.increment(this.position);
+    if(this.position == 1) this.counterService.increment(59).subscribe(counter => this.value = counter);
+    if(this.position == 2) this.counterService.increment(60).subscribe(counter => this.value = counter);
+    if(this.position == 3) this.counterService.increment(61).subscribe(counter => this.value = counter);
   }
 
 }
